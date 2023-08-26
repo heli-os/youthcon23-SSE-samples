fffd7b04ae0032c6fc08b0c6f7b31563070c587a
Taeyang Jin (Theo) <sun@dataportal.kr>
2023-08-26 14:21:27 +0900

    feat: add sseEmitter container create logics

--- src/main/java/com/example/youthcon/handson/CommentService.java
+++ src/main/java/com/example/youthcon/handson/CommentService.java
@@ -4,15 +4,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class CommentService {
     private static final String EVENT_NAME = "connect";
     private static final long EVENT_RECONNECT_TIME = 3_000L;
+    private HashMap<String, Set<SseEmitter>> container = new HashMap<>();
+
+
+    private static void sendEvent(SseEmitter emitter, SseEmitter.SseEventBuilder sseEventBuilder) {
+        try {
+            emitter.send(sseEventBuilder);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public SseEmitter connect(String articleId) {
         // 1. 새로운 Emitter 생성
-        SseEmitter emitter = new SseEmitter(300_000L);
+        SseEmitter sseEmitter = new SseEmitter(300_000L);
 
         // 2. 전송할 이벤트 작성
         SseEmitter.SseEventBuilder sseEventBuilder = SseEmitter.event()
@@ -21,11 +35,12 @@ public class CommentService {
                 .reconnectTime(EVENT_RECONNECT_TIME);
 
         // 3. 작성한 이벤트를 생성한 Emitter에 전송
-        try {
-            emitter.send(sseEventBuilder);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        sendEvent(sseEmitter, sseEventBuilder);
+
         // 4. Article과 연결된 Emitter Container 생성
+        Set<SseEmitter> existSSeEmitters = container.getOrDefault(articleId, Collections.unmodifiableSet(new HashSet<>()));
+        existSSeEmitters.add(sseEmitter);
+        container.put(articleId, existSSeEmitters);
+        return sseEmitter;
     }
 }
