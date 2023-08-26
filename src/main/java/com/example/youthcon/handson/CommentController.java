6e3a48a816fbae6d836230262aeef5f372774701
Taeyang Jin (Theo) <sun@dataportal.kr>
2023-08-26 14:11:02 +0900

    feat: add parameter in connect method

--- src/main/java/com/example/youthcon/handson/CommentController.java
+++ src/main/java/com/example/youthcon/handson/CommentController.java
@@ -1,7 +1,11 @@
 package com.example.youthcon.handson;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 public class CommentController {
@@ -12,8 +16,8 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    @GetMapping(value = "/connect")
-    public void connect() {
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect(@RequestParam("articleId") String articleId) {
         return;
     }
 }
