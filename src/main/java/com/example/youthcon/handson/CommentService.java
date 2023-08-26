6bd31892ff5fc6b6dd61f2a50e35115253997050
Taeyang Jin (Theo) <sun@dataportal.kr>
2023-08-26 14:16:53 +0900

    feat: add sseEventBuilder create logic

--- src/main/java/com/example/youthcon/handson/CommentService.java
+++ src/main/java/com/example/youthcon/handson/CommentService.java
@@ -5,11 +5,19 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 public class CommentService {
+    private static final String EVENT_NAME = "connect";
+    private static final long EVENT_RECONNECT_TIME = 3_000L;
 
     public SseEmitter connect(String articleId) {
         // 1. 새로운 Emitter 생성
         SseEmitter emitter = new SseEmitter(300_000L);
+
         // 2. 전송할 이벤트 작성
+        SseEmitter.SseEventBuilder sseEventBuilder = SseEmitter.event()
+                .name(EVENT_NAME)
+                .data("connected!")
+                .reconnectTime(EVENT_RECONNECT_TIME);
+        
         // 3. 작성한 이벤트를 생성한 Emitter에 전송
         // 4. Article과 연결된 Emitter Container 생성
     }
