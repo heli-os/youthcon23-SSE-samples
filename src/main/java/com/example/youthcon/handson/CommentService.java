0f5d55613d17660f77269201a8e1d1959bceda36
Taeyang Jin (Theo) <sun@dataportal.kr>
2023-08-26 14:23:29 +0900

    feat: extract some constants

--- src/main/java/com/example/youthcon/handson/CommentService.java
+++ src/main/java/com/example/youthcon/handson/CommentService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,8 +11,9 @@ import java.util.Set;
 @Service
 public class CommentService {
     private static final String EVENT_NAME = "connect";
-    private static final long EVENT_RECONNECT_TIME = 3_000L;
-    private HashMap<String, Set<SseEmitter>> container = new HashMap<>();
+    private static final long SSE_EMITTER_TIME_OUT_MILLIS = 1_000L;
+    private static final long EVENT_RECONNECT_TIME_MILLIS = 1_000L;
+    private final HashMap<String, Set<SseEmitter>> container = new HashMap<>();
 
 
     private static void sendEvent(SseEmitter emitter, SseEmitter.SseEventBuilder sseEventBuilder) {
@@ -26,19 +26,19 @@ public class CommentService {
 
     public SseEmitter connect(String articleId) {
         // 1. 새로운 Emitter 생성
-        SseEmitter sseEmitter = new SseEmitter(300_000L);
+        SseEmitter sseEmitter = new SseEmitter(SSE_EMITTER_TIME_OUT_MILLIS);
 
         // 2. 전송할 이벤트 작성
         SseEmitter.SseEventBuilder sseEventBuilder = SseEmitter.event()
                 .name(EVENT_NAME)
                 .data("connected!")
-                .reconnectTime(EVENT_RECONNECT_TIME);
+                .reconnectTime(EVENT_RECONNECT_TIME_MILLIS);
 
         // 3. 작성한 이벤트를 생성한 Emitter에 전송
         sendEvent(sseEmitter, sseEventBuilder);
 
         // 4. Article과 연결된 Emitter Container 생성
-        Set<SseEmitter> existSSeEmitters = container.getOrDefault(articleId, Collections.unmodifiableSet(new HashSet<>()));
+        Set<SseEmitter> existSSeEmitters = container.getOrDefault(articleId, new HashSet<>());
         existSSeEmitters.add(sseEmitter);
         container.put(articleId, existSSeEmitters);
         return sseEmitter;
