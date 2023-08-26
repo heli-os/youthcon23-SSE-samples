3508d38f3cc0b71e4de5e44c8627daa3fbb4eea4
Taeyang Jin (Theo) <sun@dataportal.kr>
2023-08-26 14:09:17 +0900

    feat: add connect path

--- src/main/java/com/example/youthcon/handson/CommentController.java
+++ src/main/java/com/example/youthcon/handson/CommentController.java
@@ -1,15 +1,7 @@
 package com.example.youthcon.handson;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import com.example.youthcon.preparation.Comment;
 
 @RestController
 public class CommentController {
@@ -20,6 +12,8 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-
-
+    @GetMapping(value = "/connect")
+    public void connect() {
+        return;
+    }
 }
