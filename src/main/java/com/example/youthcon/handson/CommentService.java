95d0439317b4e4133344af52b726b2f4435144cb
Taeyang Jin (Theo) <sun@dataportal.kr>
2023-08-26 14:14:38 +0900

    feat: add emitter instance create

--- src/main/java/com/example/youthcon/handson/CommentService.java
+++ src/main/java/com/example/youthcon/handson/CommentService.java
@@ -1,21 +1,16 @@
 package com.example.youthcon.handson;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArraySet;
-
-import com.example.youthcon.preparation.*;
-
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 public class CommentService {
 
-
+    public SseEmitter connect(String articleId) {
+        // 1. 새로운 Emitter 생성
+        SseEmitter emitter = new SseEmitter(300_000L);
+        // 2. 전송할 이벤트 작성
+        // 3. 작성한 이벤트를 생성한 Emitter에 전송
+        // 4. Article과 연결된 Emitter Container 생성
+    }
 }
